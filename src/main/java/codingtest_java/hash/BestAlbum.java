006fef3d1af2542178576dace6a493a6e41ece12
Seungki kim <ti9918@gmail.com>
2025-12-29 17:05:30 +0900

    12.29 Push & Commi (Revision and Review)

--- src/main/java/codingtest_java/hash/BestAlbum.java
+++ src/main/java/codingtest_java/hash/BestAlbum.java
@@ -15,7 +15,7 @@ class BestAlbum {
     public int[] solution(String[] genres, int[] plays) {
         // 1. 장르별 총 재생횟수
         Map<String, Integer> genrePlayCount = new HashMap<>();
-        // 2. 장르별 노래 정보
+        // 2. 장르별 노래 정보 (노래정보[고유번호 i, 재생횟수 play] 리스트로 map에 저장)
         Map<String, List<Song>> genreSongs = new HashMap<>();
 
         for (int i = 0; i < genres.length; i++) {
@@ -27,24 +27,29 @@ class BestAlbum {
             genreSongs.get(genre).add(new Song(i, play));
         }
 
-        // 3. 장르별 총 재생횟수 기준 정렬
+        // 3. 장르별 총 재생횟수 기준 정렬 ( 장르를 '총 재생횟수의 내림차순'으로 정렬 )
         List<String> sortedGenre = new ArrayList<>(genrePlayCount.keySet());
+        // (a, b) => 총 재생횟수
+        // genrePlayCount.get(b) - ... get(a) => 총 재생횟수의 내림차순으로 정렬하겠다는 뜻
         sortedGenre.sort((a, b) -> genrePlayCount.get(b) - genrePlayCount.get(a));
         // 4. 향상된 for문을 통해 장르에 저장된 노래정보(고유번호, 재생횟수)를 꺼냄.
         List<Integer> result = new ArrayList<>();
         for (String genre : sortedGenre) {
+            // 장르의 노래정보(list)를 songs에 저장
             List<Song> songs = genreSongs.get(genre);
-            // 재생횟수 내림차순, 같으면 ID 오름차순
+            // 재생횟수(play) 내림차순, 같으면 고유번호(id) 오름차순
             songs.sort((a, b) -> (a.play != b.play) ? b.play - a.play : a.id - b.id);
-            // 장르의 곡이 2곡 이하일 경우도 있을 수 있기 때문에 Math.min 함수 사용
+
+            // 장르의 곡이 2곡 미만인 경우도 고려하여 Math.min 함수 사용
             int count = Math.min(songs.size(), 2);
             for (int i = 0; i < count; i++) {
-                // result List를 초기화하고 장르의 각 곡의 고유번호를 result에 저장
+                // 이전에 초기화해둔 list result에
+                // 정렬되어있는 장르 노래의 각 고유번호를 result에 저장
                 result.add(songs.get(i).id);
             }
         }
 
-        //  stream()함수를 통해 List -> 배열 변환
+        //  List.stream().mapToInt(i->i).toArray() 함수를 통해 List -> 배열 변환
         return result.stream().mapToInt(i -> i).toArray();
 
         /* ===== for문 변환과 비교 =====
