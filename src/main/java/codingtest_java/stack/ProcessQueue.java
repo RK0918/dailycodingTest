2abcc5d88252b6e29de7ab8c379c4ff0d19ea499
Seungki kim <ti9918@gmail.com>
2026-01-04 20:04:50 +0900

    2026.01.04 Push & Commit (Revision and Review)

--- src/main/java/codingtest_java/stack/ProcessQueue.java
+++ src/main/java/codingtest_java/stack/ProcessQueue.java
@@ -22,6 +22,9 @@ public class ProcessQueue {
             boolean HigherPriority = false;
             for (Process p : queue) {
                 if (p.priority > current.priority) {
+                    // 현재 우선순위보다 que에 대기중인 프로세스의 우선순위가 높을 경우
+                    // 아래의 if문 HigherPriority을 실행시키기 위해 true 할당
+                    // HigherPriority if문으로 현재 큐를 다시 대기큐 뒤로 보냄.
                     HigherPriority = true;
                     break;
                 }
@@ -29,14 +32,39 @@ public class ProcessQueue {
             // 3. 더 높은 우선순위가 있음(true) -> 다시 큐에 넣기
             //  더 높은 우선순위가 없음(false) -> 실행순서 ++; 하여 실행했다는 것
             if (HigherPriority) {
+                // 현재 큐를 대기 큐로 다시 보냄
                 queue.offer(current);
             } else {
+                // 만약 current 우선순위가 높을경우 실행하므로 실행순서 n
                 executionOrder++;
+                // 만약 현재 큐가 location(몇 번째로 실행되는지 알고싶은 큐) 같을 경우
+                // 실행순서를 return
                 if (current.index == location) {
                     return executionOrder;
                 }
             }
         }
+        /* continue문을 통한 방법
+        while(!que.isEmpty()) {
+            Process current = que.poll();
+            // Process (0(idx), 2(priority))
+
+            boolean moved = false;
+            for (Process p : que) {
+                if (p.priority > current.priority) {
+                    moved = true;
+                    que.offer(current);
+                    break;
+                }
+            }
+            if (moved) continue;
+
+            executionOrder++;
+            if (current.idx == location) return executionOrder;
+
+        }
+
+        */
         return executionOrder;
     }
     // 프로세스 정보(원래 위치, 우선순위)
