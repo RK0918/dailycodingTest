ab30f14ed063d1df3925cbca87b48635764d2a43
Seungki kim <ti9918@gmail.com>
2026-04-29 22:47:17 +0900

    2026.04.29 refactoring & review

--- src/main/java/codingtest_java/dfs_bfs/GameShortestDistance.java
+++ src/main/java/codingtest_java/dfs_bfs/GameShortestDistance.java
@@ -3,11 +3,48 @@ package codingtest_java.dfs_bfs;
 import java.util.*;
 
 class GameShortestDistance {
-    // 동 서 남 북 (상 하 좌 우)
-    int[] dx = { -1, 1, 0,0};
-    int[] dy = { 0, 0, -1, 1};
     public int solution(int[][] maps) {
-        int n = maps.length;
+        int n = maps.length; // 행
+        int m = maps[0].length; // 열
+        // 상하좌우
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        // 방문 여부, 거리 저장
+        // distance -> 해당 좌표까지의 거리값을 구하여 저장
+        int[][] distance = new int[n][m];
+        for (int[] row : distance) Arrays.fill(row, -1);
+        // Deque -> 탐색할 좌표(위치)
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.offerLast(new int[]{0, 0});
+
+        distance[0][0] = 1; // 시작점 거리 1
+        while (!deque.isEmpty()) {
+            int[] cur = deque.pollFirst();
+            int x = cur[0];
+            int y = cur[1];
+
+            if (x == n - 1 && y == m - 1) {
+                return distance[x][y];
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (maps[nx][ny] == 0 || distance[nx][ny] != -1) continue;
+                distance[nx][ny] = distance[x][y] + 1;
+                deque.offerLast(new int[]{nx, ny});
+            }
+
+
+        }
+
+
+        return -1;
+    }
+        /*int n = maps.length;
         int m = maps[0].length;
 
         // 방문체크
@@ -62,9 +99,9 @@ class GameShortestDistance {
             }
 
         }
-        // 모든 큐를 돌았는데 목적지 도착 못했다면 -1
-        return -1;
-    }
+        모든 큐를 돌았는데 목적지 도착 못했다면 -1
+        return -1;*/
+
 
     // 💡 테스트를 위한 main 메서드 (psvm)
     public static void main(String[] args) {
@@ -93,3 +130,31 @@ class GameShortestDistance {
         System.out.println("테스트 케이스 2 실행 결과: " + sol.solution(maps2));
     }
 }
+
+/* 만약 모든 거리의 경우의 수를 모두 구해야 되며 거기서 최소거리를 반환해야 될 경우
+ dfs + dp 조합이 좋을 것
+
+ int[][] dp = new int[n][m];
+for (int[] row : dp) Arrays.fill(row, Integer.MAX_VALUE);
+
+// DFS로 모든 경로 탐색하면서 최솟값 갱신
+void dfs(int[][] maps, int[][] dp, int x, int y, int dist) {
+    if (x == n-1 && y == m-1) {
+        dp[x][y] = Math.min(dp[x][y], dist); // 도달할 때마다 최솟값 갱신
+        return;
+    }
+
+    for (int d = 0; d < 4; d++) {
+        int nx = x + dx[d];
+        int ny = y + dy[d];
+
+        if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+        if (maps[nx][ny] == 0) continue;
+        if (dist >= dp[nx][ny]) continue; // DP: 이미 더 짧은 거리면 스킵
+
+        dp[nx][ny] = dist + 1; // 현재까지 최솟값 저장
+        dfs(maps, dp, nx, ny, dist + 1);
+    }
+}
+
+ */
